f48f8a040fbbd9391d9ec584d5f6762dfb254a13
Yaroslav <jaroslav-nikiti@rambler.ru>
2022-10-08 23:15:30 +0500

    Init project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _angle;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector3 positionOffset = gameObject.transform.position;
+        Vector3 rotateOffset = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            positionOffset += Time.deltaTime * _speed * gameObject.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            positionOffset -= Time.deltaTime * _speed * gameObject.transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotateOffset = new Vector3(0f, -Time.deltaTime * _angle, 0f);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rotateOffset = new Vector3(0f, Time.deltaTime * _angle, 0f);
+        }
+
+        gameObject.transform.position = positionOffset;
+        gameObject.transform.Rotate(rotateOffset);
+    }
+}
